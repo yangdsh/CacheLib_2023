e268979659aad560a890138262c1cb9f6edff1d2
yangdsh <yangdsh@nfs.yangdsh-173064.lrbplus-pg0.utah.cloudlab.us>
2023-10-22 12:41:10 -0600

    amend test configs

--- test_throughput.sh
+++ test_throughput.sh
@@ -2,8 +2,11 @@
 GIT_HASH=$(git log --pretty=format:'%h' -n 1)
 echo $GIT_HASH
 
-CONFIG_FILES="config_kvcache config_kvcache_ML config_kvcache_ML_FIFO memc_replay memc_replay_FIFO memc_replay_ML memc_replay_ML_FIFO"
-CONFIG_FILES="memc_replay_FIFO"
+PREFIX="$PWD/opt/cachelib/"
+LD_LIBRARY_PATH="$PREFIX/lib:$PREFIX/lib64:${LD_LIBRARY_PATH:-}"
+export LD_LIBRARY_PATH
+
+CONFIG_FILES="config_kvcache config_kvcache_FIFO config_kvcache_ML config_kvcache_ML_FIFO memc_replay memc_replay_FIFO memc_replay_ML memc_replay_ML_FIFO"
 
 for CONFIG_FILE in $CONFIG_FILES
 do
