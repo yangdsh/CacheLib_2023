1d957aad0500b15d6f7b7ac806e960de7d87e9aa
yangdsh <dy5@princeton.edu>
2023-10-22 20:09:43 -0600

    dependency

--- contrib/build-package.sh
+++ contrib/build-package.sh
@@ -236,6 +236,7 @@ case "$1" in
     REPO=https://github.com/facebook/mvfst
     REPODIR=cachelib/external/$NAME
     SRCDIR=$REPODIR
+    external_git_tag=3d33c4cae380e412f1a81f193289262b668956e5
     external_git_clone=yes
     cmake_custom_params="-DBUILD_SHARED_LIBS=ON"
     ;;
